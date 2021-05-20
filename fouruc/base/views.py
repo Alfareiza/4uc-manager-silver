a1e9e79a271a4538cdb9f907974e3a86f451216e
Alfonso <alfareiza@gmail.com>
2021-05-20 20:28:44 -0300

    Inicialização de urls/views para todos os links do site.

--- fouruc/base/views.py
+++ fouruc/base/views.py
@@ -2,4 +2,76 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'base/base.html')
+    return render(request, 'base/index.html')
+
+
+def reset_pass(request):
+    return render(request, 'auth-reset-pass.html')
+
+
+def charts(request):
+    return render(request, 'charts-morris.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def maps(request):
+    return render(request, 'maps-google.html')
+
+
+def page_not_found_fourzerothree(request):
+    return render(request, 'page-403.html')
+
+
+def page_not_found_fourzerofour(request):
+    return render(request, 'page-404.html')
+
+
+def page_bad_request(request):
+    return render(request, 'page-500.html')
+
+
+def blank(request):
+    return render(request, 'page-blank.html')
+
+
+def register(request):
+    return render(request, 'register.html')
+
+
+def badges(request):
+    return render(request, 'ui-badges.html')
+
+
+def breadcrumb_pagination(request):
+    return render(request, 'ui-breadcrumb-pagination.html')
+
+
+def forms(request):
+    return render(request, 'ui-forms.html')
+
+
+def button(request):
+    return render(request, 'ui-button.html')
+
+
+def collapse(request):
+    return render(request, 'ui-collapse.html')
+
+
+def icons(request):
+    return render(request, 'ui-icons.html')
+
+
+def tables(request):
+    return render(request, 'ui-tables.html')
+
+
+def tabs(request):
+    return render(request, 'ui-tabs.html')
+
+
+def typography(request):
+    return render(request, 'ui-typography.html')
