59c748adf696ef2da78b7248f3a4e91718959e13
Alfonso <alfareiza@gmail.com>
2021-06-06 13:01:03 -0300

    decorator @login_required para todos os views, enviando no contexto valores da api de 4uc

--- fouruc/base/views.py
+++ fouruc/base/views.py
@@ -1,69 +1,74 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from fouruc.base.server.main import Client
+from decouple import config
+
+account = Client(token=config('TOKEN_4UC'))
+account.get_players()
 
 
-def home(request):
-    return render(request, 'base/index.html')
+# print(account.players)
 
-
+@login_required
 def dashboard(request):
-    return render(request, 'base/index.html')
+    return render(request, 'base/index.html', context={'name': 'Players', 'players': account.players})
 
 
-def reset_pass(request):
-    return render(request, 'auth-reset-pass.html')
-
-
+@login_required
 def charts(request):
     return render(request, 'charts-morris.html')
 
 
-def login(request):
-    return render(request, 'login.html')
-
-
+@login_required
 def maps(request):
     return render(request, 'maps-google.html')
 
 
+@login_required
 def blank(request):
     return render(request, 'page-blank.html')
 
 
-def register(request):
-    return render(request, 'register.html')
-
-
+@login_required
 def badges(request):
     return render(request, 'ui-badges.html')
 
 
+@login_required
 def breadcrumb_pagination(request):
     return render(request, 'ui-breadcrumb-pagination.html')
 
 
+@login_required
 def forms(request):
     return render(request, 'ui-forms.html')
 
 
+@login_required
 def button(request):
     return render(request, 'ui-button.html')
 
 
+@login_required
 def collapse(request):
     return render(request, 'ui-collapse.html')
 
 
+@login_required
 def icons(request):
     return render(request, 'ui-icons.html')
 
 
+@login_required
 def tables(request):
     return render(request, 'ui-tables.html')
 
 
+@login_required
 def tabs(request):
     return render(request, 'ui-tabs.html')
 
 
+@login_required
 def typography(request):
     return render(request, 'ui-typography.html')
