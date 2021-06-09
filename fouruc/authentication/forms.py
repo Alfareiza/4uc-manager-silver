70dd04d5b46dc7f4b44279616753b41cbcd2303a
Alfonso <alfareiza@gmail.com>
2021-06-09 18:56:11 -0300

    Alterado form para reconhecer só email e password1 e password2

--- fouruc/authentication/forms.py
+++ fouruc/authentication/forms.py
@@ -5,54 +5,49 @@ Copyright (c) 2019 - present AppSeed.us
 
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from fouruc.base.models import User
+
 
 class LoginForm(forms.Form):
     username = forms.CharField(
-        widget=forms.TextInput(
+        widget=forms.EmailInput(
             attrs={
-                "placeholder" : "Username",                
+                "placeholder": "Email",
                 "class": "form-control"
             }
         ))
     password = forms.CharField(
         widget=forms.PasswordInput(
             attrs={
-                "placeholder" : "Password",                
+                "placeholder": "Password",
                 "class": "form-control"
             }
         ))
 
+
 class SignUpForm(UserCreationForm):
-    username = forms.CharField(
-        widget=forms.TextInput(
-            attrs={
-                "placeholder" : "Username",                
-                "class": "form-control"
-            }
-        ))
     email = forms.EmailField(
         widget=forms.EmailInput(
             attrs={
-                "placeholder" : "Email",                
+                "placeholder": "Email",
                 "class": "form-control"
             }
         ))
     password1 = forms.CharField(
         widget=forms.PasswordInput(
             attrs={
-                "placeholder" : "Password",                
+                "placeholder": "Password",
                 "class": "form-control"
             }
         ))
     password2 = forms.CharField(
         widget=forms.PasswordInput(
             attrs={
-                "placeholder" : "Password check",                
+                "placeholder": "Password check",
                 "class": "form-control"
             }
         ))
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('email', 'password1', 'password2')
