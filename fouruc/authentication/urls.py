c43009cb179fa15cb10d2c90c49c43b130a1a79e
Alfonso <alfareiza@gmail.com>
2021-06-09 18:57:20 -0300

    Novas views referentes a recuperação da senha.

--- fouruc/authentication/urls.py
+++ fouruc/authentication/urls.py
@@ -5,10 +5,20 @@ Copyright (c) 2019 - present AppSeed.us
 
 from django.urls import path
 from .views import login_view, register_user
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LogoutView, PasswordResetView, PasswordResetConfirmView, PasswordResetDoneView, \
+    PasswordResetCompleteView
 
 urlpatterns = [
     path('login/', login_view, name="login"),
     path('register/', register_user, name="register"),
-    path("logout/", LogoutView.as_view(), name="logout")
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path('password_reset/', PasswordResetView.as_view(template_name='registration/password_reset_form.html'),
+         name='password_reset'),
+    path('password_reset/done/', PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('reset/done/', PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
