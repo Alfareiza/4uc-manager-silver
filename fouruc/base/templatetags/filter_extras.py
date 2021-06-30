692632e16162c7cc68b085fa97392f7b9906e0db
Alfonso <alfareiza@gmail.com>
2021-06-30 19:32:03 -0300

    Adicionado filter para calcular data baseado em quantidade de minutos "date_from_minute"

--- fouruc/base/templatetags/filter_extras.py
+++ fouruc/base/templatetags/filter_extras.py
@@ -1,8 +1,23 @@
 import math
-
+import datetime
 from django import template
 
 register = template.Library()
+
+
+@register.filter
+def date_from_minute(m):
+    """
+    Calc date from minutes and return a string with the date.
+    :param minutes: 351702: int
+    :return: 'Jan 25 de 2021 15:44'
+    """
+    if m is not None:
+        now = datetime.datetime.utcnow()
+        deltadate = now - datetime.timedelta(minutes=m)
+        return deltadate.strftime("%d %b %Y %H:%M")
+    else:
+        return 'Sem Informação'
 
 
 @register.filter
@@ -14,7 +29,6 @@ def minutes_in_time(minutes):
     :param minutes: 351702 :int
     :return: 'há 8 meses' :str
     '''
-    # minutes = eval(minutes)
     try:
         if minutes is not None:
             hours = minutes // 60
@@ -26,7 +40,9 @@ def minutes_in_time(minutes):
                 return f"há {year} Anos"
             elif months > 0:
                 return f"há {months} Meses"
-            elif days > 0:
+            elif days == 1:
+                return f"há {days} Día"
+            elif days > 1:
                 return f"há {days} Días"
             elif hours > 0:
                 return f"há {hours} Horas"
@@ -37,6 +53,32 @@ def minutes_in_time(minutes):
     except Exception as e:
         print('Error > ', e)
     return ''
+
+
+@register.filter
+def format_str_date(date_time_str):
+    """
+    Transform str to date
+    :param date_str: '2021-06-22 12:16:27'
+    :return:
+    """
+    if date_time_str is not None:
+        date_time_obj = datetime.datetime.strptime(date_time_str, '%Y-%m-%d %H:%M:%S')
+        date_time_obj.strftime("%d %b %Y %H:%M:%S")
+        return date_time_obj
+    else:
+        return '-'
+
+
+@register.filter
+def number_day_to_name_day(number):
+    """
+    Receives a number where 0 is sunday and returns the name of the day
+    :param number:
+    :return:
+    """
+    weekDay = {0: 'Domingo', 1: 'Segunda', 2: 'Terça', 3: 'Quarta', 4: 'Quinta', 5: 'Sexta', 6: 'Sábado', 7: 'Domingo'}
+    return weekDay[number]
 
 
 @register.filter
